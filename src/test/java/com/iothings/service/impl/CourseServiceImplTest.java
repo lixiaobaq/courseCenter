2426a03eb09daa1221e5e50c0eb070016c3b62be
Administrator <285804378@qq.com>
2020-11-09 08:48:43 +0800

    大湿最后的倔强

---
+++ src/test/java/com/iothings/service/impl/CourseServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.iothings.service.impl;
+
+import com.iothings.entity.CoursePublishEntity;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourseServiceImplTest {
+
+    @Autowired
+    private CourseServiceImpl courseService;
+
+    @Test
+    public void findAll() {
+        List<CoursePublishEntity> coursePublishEntity = courseService.findAll(10, 0, "no_project", "1", "1", "");
+        Assert.assertEquals(new ArrayList<CoursePublishEntity>(), coursePublishEntity);
+    }
+
+    @Test
+    public void findCourseAllNumbers() {
+        Integer nums = courseService.findCourseAllNumbers();
+        Assert.assertEquals(new Integer(3), nums);
+    }
+}
